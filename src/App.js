ae6c58ace17db38a30d685e6f0c2d512f51bbfa4
Tomas Gonzalez <tgonzalezv@uc.cl>
2021-08-29 14:51:26 -0400

    Add initial files :rocket:

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import "antd/dist/antd.css";
+
+import HorariosSala from './views/sala';
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+    <Switch>
+      <Route exact path="/">
+        <HorariosSala />
+      </Route>
+    </Switch>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/views/sala.js
@@ -0,0 +1,172 @@
+import React, {useState, useEffect} from 'react';
+import { Row, Col, Button, Card, Input, Form } from 'antd';
+import ScheduleSelector from 'react-schedule-selector';
+import axios from "axios";
+
+
+const style = { background: '#0092ff', padding: '8px 0' };
+// GET de array u obj, buscar fecha y obtener personas anotadas
+const example_get = [
+    {
+        'date': Date.parse(new Date("2021-08-30T05:00:00.000Z")),
+        'nombres': ['tesurot'],
+        'espacios': 1
+    }
+];
+
+export default function HorariosSala() {
+
+    const [schedule, setSchedule] = useState({});
+    const [nombre, setNombre] = useState('Tesurot');
+    const [selected, setSelected] = useState('');
+    //const [form] = Form.useForm();
+
+
+    const handleChange = (newSchedule) => {
+        setSchedule({ schedule: newSchedule });
+    }
+
+    const check_espacios = (time) => {
+        console.log(example_get[0]['date']);
+        console.log(time);
+        for(let i in example_get){
+            if(example_get[i]['date'] === Date.parse(time)){
+                return `(${example_get[i]['espacios']}/4)`;
+            }
+        }
+        return '(0/4)'
+    }
+
+    const formatTutores = (time) => {
+        let lista = [];
+        for(let i in example_get){
+            if(example_get[i]['date'] === Date.parse(time)){
+                lista = example_get[i]['nombres'];
+            }
+        }
+        let result = '';
+        for (let i in lista){
+            result += lista[i];
+        }
+        return result;
+    }
+
+    const renderCustomDateCell = (time, selected, innerRef) => (
+        <div style={{ textAlign: 'center', backgroundColor: selected ? 'rgba(89, 154, 242, 1)' : 'rgba(162, 198, 248, 1)'}} 
+            ref={innerRef} onMouseOver={(c) => {
+                c.target.style.backgroundColor = '#dbedff';
+                setSelected(formatTutores(time));
+            }}
+            onMouseLeave={(c) => {c.target.style.backgroundColor = selected ? 'rgba(89, 154, 242, 1)' : 'rgba(162, 198, 248, 1)'}}>
+            {check_espacios(time)}
+        </div>
+    )
+
+    // const validateFields = (rule, value, callback) => {
+    //     const nombre = form.getFieldValue('nombre')
+    //     if (nombre === '') {
+    //     callback('Pon tu nombre plis');
+    //     }
+
+    //     let obj = {
+    //     schedule: form.getFieldValue('schedule'),
+    //     nombre: form.getFieldValue('nombre'),
+    //     };
+    //     console.log(obj);
+    //     // let url = "https://rafa-api.herokuapp.com/people/login";
+    //     // axios
+    //     //   .post(url, obj)
+    //     //   .then((response) => {
+    //     //     console.log(response);
+    //     //     callback();
+    //     //   })
+    //     //   .catch((err) => {
+    //     //     console.log(err);
+    //     //     if (err.response) {
+    //     //       callback(err.response.data.error);
+    //     //     } else {
+    //     //       callback('Ocurrió un error.');
+    //     //     }
+    
+    //     //   });
+    // };
+    // const onFinish = (values) => {
+    //     return;
+    // };
+
+    const sendRequest = () => {
+        const obj = {schedule: schedule, nombre: nombre}
+        console.log(obj);
+        
+    }
+
+    return (
+        <div>
+        <h1>Salita Tutores 🥰</h1>
+        <Card style={{ width: 600 }}>
+            {/* <Form
+              name="basic"
+              onFinish={onFinish}
+              form={form}
+              style={{ marginTop: "10px" }}
+            > */}
+                <Col>
+                    {/* <Form.Item
+                        name="schedule"
+                        values="schedule"
+                        // dependencies={["contrasena"]}
+                        validateTrigger="onBlur"
+                        rules={[
+                        {
+                            required: true,
+                            message: "Por favor pon algun horario",
+                        },
+                        {
+                            validator: validateFields,
+                        }
+                        ]}
+                    > */}
+                        <ScheduleSelector timeFormat='h' minTime={1} maxTime={8} 
+                                        selection={schedule.schedule} onChange={handleChange}
+                                        renderDateCell={renderCustomDateCell}  dateFormat='D/M'
+                        />
+                    {/* </Form.Item> */}
+                    {/* <Form.Item
+                        name="nombre"
+                        values="nombre"
+                        // dependencies={["contrasena"]}
+                        validateTrigger="onBlur"
+                        rules={[
+                        {
+                            required: true,
+                            message: "Pon tu nombreeee",
+                        },
+                        {
+                            validator: validateFields,
+                        }
+                        ]}
+                    > */}
+                    <p style={{marginTop:'2%'}}>Tutores en la casilla: {selected}</p>
+                    <Input placeholder='Nombre Tutor/a' style={{ marginTop: '1%' }} onChange={(d) => setNombre(d.target.value)}/>
+                    {/* </Form.Item> */}
+                    <Button
+                            type="primary"
+                            size="large"
+                            // htmlType="submit"
+                            style={{
+                                textAlign: "center",
+                                marginTop: "5%",
+                                borderRadius: "10px",
+                                fontSize: "15px",
+                                color: "white"
+                            }}
+                            onClick={sendRequest}
+                        >
+                            Enviar
+                    </Button>
+                </Col>
+            {/* </Form> */}
+        </Card>
+        </div>
+    )
+}
